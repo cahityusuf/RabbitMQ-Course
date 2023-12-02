5cc8c17038ccfa4957c1e7bc8f6e37178936d3a7
cahityusuf <cahityusuf@gmail.com>
2023-12-02 14:53:34 +0300

    hello world

---
+++ Hello-World/Send/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "hello",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+
+for (int i = 0; i < 20; i++)
+{
+string message = $"Gönderilen mesaj => {i+1}";
+
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(exchange: string.Empty,
+                     routingKey: "hello",
+                     basicProperties: null,
+                     body: body); 
+
+Console.WriteLine($" [x] Sent {message}");
+}
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
